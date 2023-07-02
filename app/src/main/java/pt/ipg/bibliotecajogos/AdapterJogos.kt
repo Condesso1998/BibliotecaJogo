cf606ed5cec7f5c8fcebe6df972ac0b3a1113dc3
Rui <ruicondesso98@gmail.com>
2023-07-02 03:33:52 +0100

    Criado adaptador dos jogos (faz o "interface" com o RecyclerView)

---
+++ app/src/main/java/pt/ipg/bibliotecajogos/AdapterJogos.kt
@@ -0,0 +1,24 @@
+package pt.ipg.bibliotecajogos
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class AdapterJogos : RecyclerView.Adapter<AdapterJogos.ViewHolderJogo>() {
+    inner class ViewHolderJogo(itemView: View) : ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderJogo {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderJogo, position: Int) {
+        TODO("Not yet implemented")
+    }
+}

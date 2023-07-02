78308f7cca25305fa49d74f38a738ccc098d7133
Rui <ruicondesso98@gmail.com>
2023-07-02 02:37:35 +0100

    Adicionada data de publicação à tabela de jogos

--- app/src/main/java/pt/ipg/bibliotecajogos/Jogo.kt
+++ app/src/main/java/pt/ipg/bibliotecajogos/Jogo.kt
@@ -1,9 +1,13 @@
 package pt.ipg.bibliotecajogos
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+import java.util.Calendar
 
 data class Jogo(
     var titulo: String,
+    var dataPublicacao: Calendar? = null,
     var idCategoria: Long,
     var id: Long = -1
 ) {
@@ -11,8 +15,34 @@ data class Jogo(
         val valores = ContentValues()
 
         valores.put(TabelaJogos.CAMPO_TITULO, titulo)
+        valores.put(TabelaJogos.CAMPO_DATA_PUB, dataPublicacao?.timeInMillis)
         valores.put(TabelaJogos.CAMPO_FK_CATEGORIA, idCategoria)
 
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor) : Jogo {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posTitulo = cursor.getColumnIndex(TabelaJogos.CAMPO_TITULO)
+            val posDataPub = cursor.getColumnIndex(TabelaJogos.CAMPO_DATA_PUB)
+            val posCategoriaFK = cursor.getColumnIndex(TabelaJogos.CAMPO_FK_CATEGORIA)
+
+            val id = cursor.getLong(posId)
+            val titulo = cursor.getString(posTitulo)
+
+            var dataPub: Calendar?
+
+            if (cursor.isNull(posDataPub)) {
+                dataPub = null
+            } else {
+                dataPub = Calendar.getInstance()
+                dataPub.timeInMillis = cursor.getLong(posDataPub)
+            }
+
+            val categoriaId = cursor.getLong(posCategoriaFK)
+
+            return Jogo(titulo, dataPub, categoriaId, id)
+        }
     }
 }
--- app/src/main/java/pt/ipg/bibliotecajogos/TabelaJogos.kt
+++ app/src/main/java/pt/ipg/bibliotecajogos/TabelaJogos.kt
@@ -8,6 +8,7 @@ class TabelaJogos(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
         db.execSQL("CREATE TABLE " + NOME_TABELA + " (" +
                 "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "titulo TEXT NOT NULL, " +
+                "$CAMPO_DATA_PUB INTEGER"+
                 "id_categoria INTEGER NOT NULL, " +
                 "FOREIGN KEY (id_categoria) REFERENCES " + TabelaCategorias.NOME_TABELA +
                 "(" + BaseColumns._ID + ") ON DELETE RESTRICT)");
@@ -17,6 +18,9 @@ class TabelaJogos(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     companion object {
         const val NOME_TABELA = "jogos"
         const val CAMPO_TITULO = "titulo"
+        const val CAMPO_DATA_PUB = "data_publicacao"
         const val CAMPO_FK_CATEGORIA = "id_categoria"
+
+        val CAMPOS = arrayOf(BaseColumns._ID, CAMPO_TITULO, CAMPO_DATA_PUB, CAMPO_FK_CATEGORIA)
     }
 }

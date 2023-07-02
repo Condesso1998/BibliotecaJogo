d30ddaf965f20cd44895760b09d52dcb80e4b91a
Rui <ruicondesso98@gmail.com>
2023-07-02 03:14:19 +0100

    Mudar o nome dos fragmentos

--- app/src/main/java/pt/ipg/bibliotecajogos/SobreFragment.kt
+++ app/src/main/java/pt/ipg/bibliotecajogos/SobreFragment.kt
@@ -11,7 +11,7 @@ import pt.ipg.bibliotecajogos.databinding.FragmentSecondBinding
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SobreFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 

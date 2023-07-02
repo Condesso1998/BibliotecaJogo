0168070851bf4acdd02cbc665f002344978c655b
Rui <ruicondesso98@gmail.com>
2023-07-02 15:50:54 +0100

    Processar as opções do menu da lista de livros no fragmento correspondente

--- app/src/main/java/pt/ipg/bibliotecajogos/ListaJogosFragment.kt
+++ app/src/main/java/pt/ipg/bibliotecajogos/ListaJogosFragment.kt
@@ -4,6 +4,7 @@ import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.loader.app.LoaderManager
@@ -81,5 +82,35 @@ class ListaJogosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
     override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
         adapterJogos!!.cursor = data
+    }
+
+    fun processaOpcaoMenu(item: MenuItem) : Boolean {
+        return when (item.itemId) {
+            R.id.action_adicionar -> {
+                adicionaLivro()
+                true
+            }
+            R.id.action_editar -> {
+                editarLivro()
+                true
+            }
+            R.id.action_eliminar -> {
+                eliminarLivro()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun eliminarLivro() {
+
+    }
+
+    private fun editarLivro() {
+
+    }
+
+    private fun adicionaLivro() {
+
     }
 }

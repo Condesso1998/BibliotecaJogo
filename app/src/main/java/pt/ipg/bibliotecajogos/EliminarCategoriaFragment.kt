5c73b2482caf030d64c9448d4a5be581872ab999
Rui <ruicondesso98@gmail.com>
2023-07-04 16:49:11 +0100

    Adicionada fragmento para eliminar categorias e correspondente menu

---
+++ app/src/main/java/pt/ipg/bibliotecajogos/EliminarCategoriaFragment.kt
@@ -0,0 +1,62 @@
+package pt.ipg.bibliotecajogos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.ipg.bibliotecajogos.databinding.FragmentEliminarCategoriaBinding
+
+class EliminarCategoriaFragment : Fragment() {
+    private var _binding: FragmentEliminarCategoriaBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentEliminarCategoriaBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_eliminar
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_eliminar -> {
+                eliminar()
+                true
+            }
+            R.id.action_cancelar -> {
+                voltaListaLivros()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun voltaListaLivros() {
+        findNavController().navigate(R.id.action_eliminarCategoriaFragment_to_listaCategoriasFragment)
+    }
+
+    private fun eliminar() {
+    }
+}
